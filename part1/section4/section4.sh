4668fd9c37ad6b68db7cba22df49e3efaf23b6d6
dhoui <mdhouib195@gmail.com>
2024-01-15 13:29:04 +0100

    adding certificates + readme files

---
+++ part1/section4/section4.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Function to install OpenVPN
+installOpenVpn() {
+    sudo apt-get update
+    sudo apt-get install openvpn
+    sudo apt-get install libpam-ldap
+    sudo apt-get install openvpn-auth-ldap
+}
+
+# Function to configure OpenVPN server
+configureOpenVpn() {
+  sudo openssl dhparam -out /etc/openvpn/dh2048.pem 2048
+    # Edit OpenVPN server configuration file
+    sudo cp openvpn-server.conf /etc/openvpn/server.conf
+
+    sudo cp openvpn-client.conf /etc/openvpn/client.conf
+
+    # Create LDAP configuration directory
+    sudo mkdir -p /etc/openvpn/auth
+
+    # Copy LDAP configuration file
+    sudo cp ldap.conf /etc/openvpn/auth/ldap.conf
+
+    # Restart OpenVPN service
+    sudo service openvpn restart
+}
+
+
+
+
+# Function to test VPN connection with OpenLDAP credentials
+testVpnClient() {
+    # Prompt the user for LDAP credentials
+    read -p "Enter LDAP username: " ldapUsername
+    read -s -p "Enter LDAP password: " ldapPassword
+    echo  # Move to the next line after password input
+
+    # Connect to VPN using the provided LDAP credentials
+    echo -e "$ldapUsername\n$ldapPassword" | sudo openvpn --config /etc/openvpn/client.conf --auth-user-pass /dev/stdin
+}
+
+## Function to test for authorized and unauthorized clients
+#testClient() {
+    # Test an authorized client (ensure LDAP entry is allowed)
+    # Test an unauthorized client (ensure LDAP entry is not allowed)
+#}
